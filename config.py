88b2ef633b33b2c7d0b3c2ef06230776aeceaa6b
PILSUCHOI <chlvlftn22@gmail.com>
2026-03-05 23:49:44 +0900

    config 파일 추가

---
+++ config.py
@@ -0,0 +1,4 @@
+eval_result_csv_path = "eval/results/eval_results.csv"
+eval_result_json_path = "eval/results/eval_results.json"
+test_set_path = "example_dataset/test_set.json"
+test_corpus_path = "example_dataset/corpus.json"
