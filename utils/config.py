ee774c459edaf7550857f82210f41c427593a342
Phil <pschoi@agilesoda.ai>
2026-03-06 15:08:21 +0900

    config 값 추가

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,10 @@
-eval_result_csv_path = "eval/results/eval_results.csv"
-eval_result_json_path = "eval/results/eval_results.json"
-test_set_path = "example_dataset/test_set.json"
-test_corpus_path = "example_dataset/corpus.json"
+eval_rag_pipeline_result_csv_path = "eval/results/eval_rag_pipeline_results.csv"
+eval_rag_pipeline_result_json_path = "eval/results/eval_rag_pipeline_results.json"
+eval_full_text_search_result_csv_path = "eval/results/eval_full_text_search_results.csv"
+eval_full_text_search_result_json_path = (
+    "eval/results/eval_full_text_search_results.json"
+)
+test_rag_pipeline_set_path = "genarate_testsets/files/rag/testsets/"  # test_set.json"  # "example_dataset/test_rag_pipeline_set.json"
+test_full_text_search_set_path = "genarate_testsets/files/fts/testsets/"  # test_set.json"  # "example_dataset/test_full_text_search_set.json"
+test_rag_pipeline_corpus_path = "example_dataset/corpus.json"
+SERVER_URL = "http://localhost:8003/api/search"
